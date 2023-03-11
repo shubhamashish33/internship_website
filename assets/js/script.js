d4e79ddde58863a4097621183c4f731d7294338a
Shubham Ashish <shubhamashish8@gmail.com>
2023-03-11 18:14:23 +0530

    merged all the files

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+Javascript: (function() {
+    document.getElementById('username').value = 'admin';
+    document.getElementById('password').value = 'admin';
+    document.forms.loginForma.submit.click();
+})();
